7441085aa6bb2ca18f926a6426f933c94fcbfa99
shogo4405 <shogo4405@gmail.com>
2021-06-07 02:58:32 +0900

    Update document.

--- docs/scripts/sourceset_dependencies.js
+++ docs/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":haishinkit:dokkaHtml/androidTestRelease": [], ":haishinkit:dokkaHtml/debug": [], ":haishinkit:dokkaHtml/main": [], ":haishinkit:dokkaHtml/release": []}'
+sourceset_dependencies='{":haishinkit:dokkaHtml/androidTestRelease":[],":haishinkit:dokkaHtml/debug":[],":haishinkit:dokkaHtml/main":[],":haishinkit:dokkaHtml/release":[]}'
