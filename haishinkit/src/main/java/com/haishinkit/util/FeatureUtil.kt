e96a43d0808e44bcbfbaf95e11bbcc0e1f0b9a36
shogo4405 <shogo4405@gmail.com>
2022-02-11 19:26:43 +0900

    Update Feature flag classes.

--- haishinkit/src/main/java/com/haishinkit/util/FeatureUtil.kt
+++ haishinkit/src/main/java/com/haishinkit/util/FeatureUtil.kt
@@ -1,13 +1,14 @@
 package com.haishinkit.util
 
+import org.json.JSONObject
 import java.util.concurrent.ConcurrentHashMap
 
 /**
  * The util object to get feature flag info.
  */
 object FeatureUtil {
-    const val FEATURE_BITRATE_CHANGE = 0
-    const val FEATURE_VULKAN_PIXEL_TRANSFORM = 1
+    const val FEATURE_BITRATE_CHANGE = "bitrate_change"
+    const val FEATURE_VULKAN_PIXEL_TRANSFORM = "vulkan_pixel_transform"
 
     private var flags = ConcurrentHashMap(
         mutableMapOf(
@@ -19,14 +20,18 @@ object FeatureUtil {
     /**
      * Whether or not a flag is enabled.
      */
-    fun isEnabled(feature: Int): Boolean {
+    fun isEnabled(feature: String): Boolean {
         return flags[feature] ?: false
     }
 
     /**
      * Setter for feature flag.
      */
-    fun setEnabled(feature: Int, isEnabled: Boolean) {
+    fun setEnabled(feature: String, isEnabled: Boolean) {
         flags[feature] = isEnabled
+    }
+
+    override fun toString(): String {
+        return JSONObject(flags.toMap()).toString()
     }
 }
