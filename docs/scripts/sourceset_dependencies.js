93b6d19b7f441346e4c5771f524d908a3101caff
shogo4405 <shogo4405@gmail.com>
2022-02-21 21:58:42 +0900

    Bump to up 0.6.0

--- docs/scripts/sourceset_dependencies.js
+++ docs/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies='{":haishinkit:dokkaHtml/androidTestRelease":[],":haishinkit:dokkaHtml/debug":[],":haishinkit:dokkaHtml/main":[],":haishinkit:dokkaHtml/release":[]}'
+sourceset_dependencies='{":haishinkit:dokkaHtml/androidTestRelease":[],":haishinkit:dokkaHtml/debug":[],":haishinkit:dokkaHtml/main":[],":haishinkit:dokkaHtml/release":[],":haishinkit:dokkaHtml/testFixtures":[],":haishinkit:dokkaHtml/testFixturesDebug":[],":haishinkit:dokkaHtml/testFixturesRelease":[]}'
