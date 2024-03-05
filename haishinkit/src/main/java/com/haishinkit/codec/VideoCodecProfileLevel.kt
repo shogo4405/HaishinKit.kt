451f04034c59c7678cb25eaf506a78e3ec0b768b
shogo4405 <shogo4405@gmail.com>
2024-03-06 01:00:20 +0900

    Add VP9 profile levels.

--- haishinkit/src/main/java/com/haishinkit/codec/VideoCodecProfileLevel.kt
+++ haishinkit/src/main/java/com/haishinkit/codec/VideoCodecProfileLevel.kt
@@ -252,6 +252,430 @@ enum class VideoCodecProfileLevel(
         MediaCodecInfo.CodecProfileLevel.HEVCMainTierLevel62
     ),
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE0_1(
+        "VP9_Profile0_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile0,
+        MediaCodecInfo.CodecProfileLevel.VP9Level1
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE0_11(
+        "VP9_Profile0_1_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile0,
+        MediaCodecInfo.CodecProfileLevel.VP9Level11
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE0_2(
+        "VP9_Profile0_2",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile0,
+        MediaCodecInfo.CodecProfileLevel.VP9Level2
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE0_21(
+        "VP9_Profile0_2_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile0,
+        MediaCodecInfo.CodecProfileLevel.VP9Level21
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE0_3(
+        "VP9_Profile0_3",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile0,
+        MediaCodecInfo.CodecProfileLevel.VP9Level3
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE0_31(
+        "VP9_Profile0_3_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile0,
+        MediaCodecInfo.CodecProfileLevel.VP9Level31
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE0_4(
+        "VP9_Profile0_4",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile0,
+        MediaCodecInfo.CodecProfileLevel.VP9Level4
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE0_41(
+        "VP9_Profile0_4_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile0,
+        MediaCodecInfo.CodecProfileLevel.VP9Level41
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE0_5(
+        "VP9_Profile0_5",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile0,
+        MediaCodecInfo.CodecProfileLevel.VP9Level5
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE0_51(
+        "VP9_Profile0_5_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile0,
+        MediaCodecInfo.CodecProfileLevel.VP9Level51
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE0_52(
+        "VP9_Profile0_5_2",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile0,
+        MediaCodecInfo.CodecProfileLevel.VP9Level52
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE0_6(
+        "VP9_Profile0_6",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile0,
+        MediaCodecInfo.CodecProfileLevel.VP9Level6
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE0_61(
+        "VP9_Profile0_6_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile0,
+        MediaCodecInfo.CodecProfileLevel.VP9Level61
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE0_62(
+        "VP9_Profile0_6_2",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile0,
+        MediaCodecInfo.CodecProfileLevel.VP9Level62
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE1_1(
+        "VP9_Profile1_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile1,
+        MediaCodecInfo.CodecProfileLevel.VP9Level1
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE1_11(
+        "VP9_Profile1_1_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile1,
+        MediaCodecInfo.CodecProfileLevel.VP9Level11
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE1_2(
+        "VP9_Profile1_2",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile1,
+        MediaCodecInfo.CodecProfileLevel.VP9Level2
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE1_21(
+        "VP9_Profile1_2_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile1,
+        MediaCodecInfo.CodecProfileLevel.VP9Level21
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE1_3(
+        "VP9_Profile1_3",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile1,
+        MediaCodecInfo.CodecProfileLevel.VP9Level3
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE1_31(
+        "VP9_Profile1_3_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile1,
+        MediaCodecInfo.CodecProfileLevel.VP9Level31
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE1_4(
+        "VP9_Profile1_4",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile1,
+        MediaCodecInfo.CodecProfileLevel.VP9Level4
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE1_41(
+        "VP9_Profile1_4_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile1,
+        MediaCodecInfo.CodecProfileLevel.VP9Level41
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE1_5(
+        "VP9_Profile1_5",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile1,
+        MediaCodecInfo.CodecProfileLevel.VP9Level5
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE1_51(
+        "VP9_Profile1_5_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile1,
+        MediaCodecInfo.CodecProfileLevel.VP9Level51
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE1_6(
+        "VP9_Profile1_6",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile0,
+        MediaCodecInfo.CodecProfileLevel.VP9Level6
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE1_61(
+        "VP9_Profile1_6_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile1,
+        MediaCodecInfo.CodecProfileLevel.VP9Level61
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE1_62(
+        "VP9_Profile1_6_2",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile1,
+        MediaCodecInfo.CodecProfileLevel.VP9Level62
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE2_1(
+        "VP9_Profile2_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile2,
+        MediaCodecInfo.CodecProfileLevel.VP9Level1
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE2_11(
+        "VP9_Profile2_1_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile2,
+        MediaCodecInfo.CodecProfileLevel.VP9Level11
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE2_2(
+        "VP9_Profile2_2",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile2,
+        MediaCodecInfo.CodecProfileLevel.VP9Level2
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE2_21(
+        "VP9_Profile2_2_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile2,
+        MediaCodecInfo.CodecProfileLevel.VP9Level21
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE2_3(
+        "VP9_Profile2_3",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile2,
+        MediaCodecInfo.CodecProfileLevel.VP9Level3
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE2_31(
+        "VP9_Profile2_3_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile2,
+        MediaCodecInfo.CodecProfileLevel.VP9Level31
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE2_4(
+        "VP9_Profile2_4",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile2,
+        MediaCodecInfo.CodecProfileLevel.VP9Level4
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE2_41(
+        "VP9_Profile2_4_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile2,
+        MediaCodecInfo.CodecProfileLevel.VP9Level41
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE2_5(
+        "VP9_Profile2_5",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile2,
+        MediaCodecInfo.CodecProfileLevel.VP9Level5
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE2_51(
+        "VP9_Profile2_5_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile2,
+        MediaCodecInfo.CodecProfileLevel.VP9Level51
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE2_6(
+        "VP9_Profile2_6",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile2,
+        MediaCodecInfo.CodecProfileLevel.VP9Level6
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE2_61(
+        "VP9_Profile2_6_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile2,
+        MediaCodecInfo.CodecProfileLevel.VP9Level61
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE2_62(
+        "VP9_Profile2_6_2",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile2,
+        MediaCodecInfo.CodecProfileLevel.VP9Level62
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE3_1(
+        "VP9_Profile3_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile3,
+        MediaCodecInfo.CodecProfileLevel.VP9Level1
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE3_11(
+        "VP9_Profile3_1_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile3,
+        MediaCodecInfo.CodecProfileLevel.VP9Level11
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE3_2(
+        "VP9_Profile3_2",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile3,
+        MediaCodecInfo.CodecProfileLevel.VP9Level2
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE3_21(
+        "VP9_Profile3_2_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile3,
+        MediaCodecInfo.CodecProfileLevel.VP9Level21
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE3_3(
+        "VP9_Profile3_3",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile3,
+        MediaCodecInfo.CodecProfileLevel.VP9Level3
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE3_31(
+        "VP9_Profile3_3_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile3,
+        MediaCodecInfo.CodecProfileLevel.VP9Level31
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE3_4(
+        "VP9_Profile3_4",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile3,
+        MediaCodecInfo.CodecProfileLevel.VP9Level4
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE3_41(
+        "VP9_Profile3_4_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile3,
+        MediaCodecInfo.CodecProfileLevel.VP9Level41
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE3_5(
+        "VP9_Profile3_5",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile3,
+        MediaCodecInfo.CodecProfileLevel.VP9Level5
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE3_51(
+        "VP9_Profile3_5_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile3,
+        MediaCodecInfo.CodecProfileLevel.VP9Level51
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE3_6(
+        "VP9_Profile3_6",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile3,
+        MediaCodecInfo.CodecProfileLevel.VP9Level6
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE3_61(
+        "VP9_Profile3_6_1",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile3,
+        MediaCodecInfo.CodecProfileLevel.VP9Level61
+    ),
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    VP9_PROFILE3_62(
+        "VP9_Profile3_6_2",
+        MediaFormat.MIMETYPE_VIDEO_VP9,
+        MediaCodecInfo.CodecProfileLevel.VP9Profile3,
+        MediaCodecInfo.CodecProfileLevel.VP9Level62
+    ),
+
     @RequiresApi(Build.VERSION_CODES.Q)
     AV1_MAIN8_2(
         "AV1_Main8_2",
